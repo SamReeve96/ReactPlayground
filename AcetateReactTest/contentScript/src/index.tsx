e5ef4abe39e110b4aaa06e763483fd7dae04e370
Samuel Reeve <sreeve96@googlemail.com>
2020-07-03 19:20:21 +0100

    Create React app for content script to test cards

---
+++ AcetateReactTest/contentScript/src/index.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ========================
+// React components
+// ========================
+
+function CardsContainer(props: any) {
+    const [annotations, setAnnotations] = React.useState(props.storageAnnotations as annotation[]);
+
+    let newAnnotation: annotation = {
+        id: annotations.length,
+        comment: 'Blam',
+        created: Date.now().toString()
+    }
+
+    function addDummyAnnotation() {
+        setAnnotations(annotations.concat([newAnnotation]));
+    }
+
+    const cards = annotations.map((annotation) => {
+        return (
+            <AnnotationCard
+                annotationData={annotation}
+                key={annotation.id}
+            />
+        )
+    })
+
+    return (
+        <div id='shadowDiv'>
+            <h1>Hello, Lets test the cards container</h1>
+            <button
+                id='createNewAnnotation'
+                onClick={() => addDummyAnnotation()}
+            >
+                Create new annotation
+            </button>
+            <ol>
+                {cards}
+            </ol>
+        </div>
+    );
+}
+
+function AnnotationCard(props: any) {
+    const annotationData: annotation = props.annotationData;
+    return (
+        <li className='annotationCard'>
+            <p>id: {annotationData.id}</p>
+            <p>Comment: {annotationData.comment}</p>
+            <p>Created: {annotationData.created}</p>
+        </li>
+    )
+}
+
+// ========================
+// Logic
+// ========================
+
+type annotation = {
+    id: number,
+    comment: string,
+    created: string
+}
+
+let emulatedStorageAnnotations: annotation[] = [];
+
+// Add a couple fake annotations to the emulated storage
+for (let i: number = 0; i < 5; i++) {
+    let newAnnotation: annotation = {
+        id: i,
+        comment: 'Blam',
+        created: Date.now().toString()
+    }
+    emulatedStorageAnnotations.push(newAnnotation);
+}
+
+// Create a shadow DOM
+function createCardContainer() {
+    const shadowContainer = checkNullableObject(document.querySelector('body'));
+    const shadow = shadowContainer.attachShadow({ mode: 'open' });
+
+    // Render react components inside shadow dom
+    ReactDOM.render(
+        <CardsContainer
+            storageAnnotations={emulatedStorageAnnotations}
+        />,
+        shadow
+    );
+
+    //Import styling for shadow dom
+    const shadowDiv = shadow.querySelector('#shadowDiv');
+    const cardsContainerCssURL = './index.css';
+    fetch(cardsContainerCssURL).then(response => response.text()).then(data => {
+        shadowDiv.insertAdjacentHTML('afterbegin', `<style> ${data} </style>`);
+    });
+}
+
+// Nullable object checker (For typescript Vs. Document object)
+function checkNullableObject(nullableObject: any) {
+    if (nullableObject === null) {
+        console.error('Object was found to be null, it cant be: ', nullableObject);
+    } else {
+        return nullableObject;
+    }
+}
+
+createCardContainer();
